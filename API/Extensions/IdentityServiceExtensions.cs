29761d311c18b30630726b7d53cd2c3ac115184c
Changsoo cho <changsoo.charles.cho@gmail.com>
2022-09-27 23:27:53 -0400

    feat: refactor codes

--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.IdentityModel.Tokens;
 
 namespace API.Extensions
@@ -12,6 +15,16 @@ namespace API.Extensions
     {
         public static IServiceCollection AddIdentityServices(this IServiceCollection services, IConfiguration config)
         {
+            services.AddIdentityCore<AppUser>(opt =>
+            {
+                opt.Password.RequireNonAlphanumeric = false;
+            })
+                .AddRoles<AppRole>()
+                .AddRoleManager<RoleManager<AppRole>>()
+                .AddSignInManager<SignInManager<AppUser>>()
+                .AddRoleValidator<RoleValidator<AppRole>>()
+                .AddEntityFrameworkStores<DataContext>();
+
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
            .AddJwtBearer(options =>
            {
@@ -29,7 +42,7 @@ namespace API.Extensions
                    {
                        var accessToken = context.Request.Query["access_token"];
                        var path = context.HttpContext.Request.Path;
-                       if (!string.IsNullOrEmpty(accessToken) && 
+                       if (!string.IsNullOrEmpty(accessToken) &&
                             path.StartsWithSegments("/hubs"))
                        {
                            context.Token = accessToken;
@@ -37,6 +50,12 @@ namespace API.Extensions
                        return Task.CompletedTask;
                    }
                };
+           });
+
+            services.AddAuthorization(opt =>
+           {
+               opt.AddPolicy("RequireAdminRole", policy => policy.RequireRole("Admin"));
+               opt.AddPolicy("ModeratePhotoRole", policy => policy.RequireRole("Admin", "Moderator"));
            });
 
             return services;
--- API/Services/TokenService.cs
+++ API/Services/TokenService.cs
@@ -10,18 +10,21 @@ using API.interfaces;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.Extensions.Configuration;
 using System.Security.Cryptography;
+using Microsoft.AspNetCore.Identity;
 
 namespace API.Services
 {
     public class TokenService : ITokenService
     {
         private readonly SymmetricSecurityKey _key;
-        public TokenService(IConfiguration config)
+        private readonly UserManager<AppUser> _userManager;
+        public TokenService(IConfiguration config, UserManager<AppUser> userManager)
         {
+            _userManager = userManager;
             _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
         }
 
-        public string CreateToken(AppUser user)
+        public async Task<string> CreateToken(AppUser user)
         {
 
             // Add claim
@@ -30,6 +33,10 @@ namespace API.Services
                 new Claim(JwtRegisteredClaimNames.NameId, user.Id.ToString()),
                 new Claim(JwtRegisteredClaimNames.UniqueName, user.UserName),
             };
+
+            var roles = await _userManager.GetRolesAsync(user);
+
+            claims.AddRange(roles.Select(role => new Claim(ClaimTypes.Role, role)));
 
             //create credentials
             var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
