fb5bd8b3e24eceacb8d55c35de482f380dec4dbc
Changsoo cho <changsoo.charles.cho@gmail.com>
2023-03-06 21:52:16 -0500

    feat: update main profile page

--- client/src/app/profile/profile.component.ts
+++ client/src/app/profile/profile.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../_models/user';
+import { AccountService } from '../_services/account.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent implements OnInit {
-
-  constructor() { }
+  user?: User;
+  constructor(private accountService: AccountService) {}
 
   ngOnInit(): void {
+    this.accountService.currentUser$.subscribe((res) => {
+      this.user = res!;
+    });
   }
-
 }
